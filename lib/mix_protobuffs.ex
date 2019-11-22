0c4de3b27503705db1f095c21db9ae1ad0d8efe7
Roger Lipscombe <roger@differentpla.net>
2019-11-22 17:51:40 +0000

    Initial creation

---
+++ lib/mix_protobuffs.ex
@@ -0,0 +1,18 @@
+defmodule MixProtobuffs do
+  @moduledoc """
+  Documentation for MixProtobuffs.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MixProtobuffs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mix_protobuffs_test.exs
@@ -0,0 +1,8 @@
+defmodule MixProtobuffsTest do
+  use ExUnit.Case
+  doctest MixProtobuffs
+
+  test "greets the world" do
+    assert MixProtobuffs.hello() == :world
+  end
+end
